6d4690d37893f15043df82c934c11b257c137a5f
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2025-08-31 20:14:18 +0330

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/App.scss";
+import "./styles/app.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
